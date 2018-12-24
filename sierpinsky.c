13db604d53ce8eb6fa65b52fb3ded47068955192
Matus Stas <matusstas.ms@gmail.com>
2018-12-24 22:42:48 +0100

    code is done

--- sierpinsky.c
+++ sierpinsky.c
@@ -1,17 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int size_of_array(int *iteration)
+void size_of_array(int *iteration)
 {
 	*iteration = 2 << (*iteration - 1); 
 }
 
 
-int make_2d_array(int *arr[], int *iteration)
+ void make_2d_array(int *arr[], int *iteration)
 {
 	for(int i = 0; i < *iteration; i++)
 		arr[i] = (int *) malloc((i+1) * sizeof(int));
-	(*iteration)++;
+}
+
+
+int is_even(int number)
+{
+	if(number%2 == 0)
+		return 1;
+	return 0;
+}
+
+
+void fill_2d_array(int *arr[], int *iteration)
+{
+	int number;
+	for(int i = 0; i < *iteration; i++)
+		for(int j = 0; j < i+1; j++)
+			if(j == 0 || j == i)
+				*(*(arr + i) + j) = 1;
+			else
+				{
+					number = (*(*(arr + i-1) + j)) + (*(*(arr + i-1) + (j-1)));
+					*(*(arr + i) + j) = number;
+				}
+}
+
+
+void show_sierpinsky_triangle(int *arr[], int *iteration)
+{
+	int number;
+	for(int i = 0; i < *iteration; i++)
+	{
+		for(int j = 0; j < i+1; j++)
+		{
+			number = *(*(arr + i) + j);
+			if(is_even(number))
+				printf("%c ", ' ');
+			else{printf("%c ", '*');}
+		}
+		putchar('\n');
+	}
 }
 
 
@@ -23,6 +62,7 @@ int main()
 
 	int *arr[iteration];
 	make_2d_array(&*arr, &iteration);
-	
+	fill_2d_array(&*arr, &iteration);
+	show_sierpinsky_triangle(&*arr, &iteration);
 	return 0;
 }
