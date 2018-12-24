4675d152783f2e434be92cd1720c2beacdc73d51
Matus Stas <matusstas.ms@gmail.com>
2018-12-24 22:17:06 +0100

    Added function which makes dynamic array of pointers -> 2d array. I chose array of pointers due to of memory saving. With this method we saved half of basic rectangle 2d array because we need just triangle shape array

--- sierpinsky.c
+++ sierpinsky.c
@@ -7,12 +7,22 @@ int size_of_array(int *iteration)
 }
 
 
+int make_2d_array(int *arr[], int *iteration)
+{
+	for(int i = 0; i < *iteration; i++)
+		arr[i] = (int *) malloc((i+1) * sizeof(int));
+	(*iteration)++;
+}
+
 
 int main()
 {
 	int iteration;
 	scanf("%d",&iteration);
 	size_of_array(&iteration);
-	printf("%d\n", iteration);
+
+	int *arr[iteration];
+	make_2d_array(&*arr, &iteration);
+	
 	return 0;
 }
